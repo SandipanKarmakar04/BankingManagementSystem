9933695c7e42b6f2117cac696aff2ae561d85f4c
Sandipan <sandipan.sk7@gmail.com>
2026-04-10 03:51:16 +0530

    Third commit - Banking Management System

--- src/in/bank/main/Main.java
+++ src/in/bank/main/Main.java
@@ -3,70 +3,119 @@ package in.bank.main;
 import java.util.Scanner;
 
 import in.bank.bankingDAO.BankingDAO;
+import in.bank.bankService.BankService;
+
 
 public class Main {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        BankingDAO dao = new BankingDAO();
+		Scanner sc = new Scanner(System.in);
+		BankingDAO dao = new BankingDAO();
 
-        while (true) {
-        	System.out.println("=================================");
-        	System.out.println("   BANKING MANAGEMENT SYSTEM");
-        	System.out.println("=================================");
-            System.out.println("1. Register");
-            System.out.println("2. Login");
-            System.out.println("3. Exit");
-            System.out.print("Enter choice: ");
-            
-            int choice = sc.nextInt();
-            sc.nextLine(); 
+		while (true) {
+			System.out.println("=================================");
+			System.out.println("   BANKING MANAGEMENT SYSTEM");
+			System.out.println("=================================");
+			System.out.println("1. Register");
+			System.out.println("2. Login");
+			System.out.println("3. Exit");
+			System.out.print("Enter choice: ");
 
-            switch (choice) {
+			int choice = sc.nextInt();
+			sc.nextLine();
 
-                case 1:
-                    System.out.print("Enter Name: ");
-                    String name = sc.nextLine();
+			switch (choice) {
 
-                    System.out.print("Email: ");
-                    String email = sc.nextLine();
+			case 1:
+				System.out.print("Enter Name: ");
+				String name = sc.nextLine();
 
-                    System.out.print("Password: ");
-                    String pass = sc.nextLine();
+				System.out.print("Email: ");
+				String email = sc.nextLine();
 
-                    if (dao.register(name, email, pass))
-                        System.out.println("Registered Successfully!");
-                    else
-                        System.out.println("Error!");
+				System.out.print("Password: ");
+				String pass = sc.nextLine();
 
-                    break;
+				if (dao.register(name, email, pass))
+					System.out.println("Registered Successfully!");
+				else
+					System.out.println("Error!");
 
-                case 2:
-                    System.out.print("Email: ");
-                    email = sc.next();
+				break;
 
-                    System.out.print("Password: ");
-                    pass = sc.next();
+			case 2:
+				System.out.print("Email: ");
+				email = sc.next();
 
-                    int userId = dao.login(email, pass);
+				System.out.print("Password: ");
+				pass = sc.next();
 
-                    if (userId != -1) {
-                        System.out.println("Login Successful!");
+				int userId = dao.login(email, pass);
 
-                        System.out.print("Set PIN: ");
-                        int pin = sc.nextInt();
+				if (userId != -1) {
+					System.out.println("Login Successful!");
 
-                        dao.createAccount(userId, pin);
-                        System.out.println("Account Created!");
+					while (true) {
+						System.out.println("\n===== BANK MENU =====");
+						System.out.println("1. Deposit");
+						System.out.println("2. Withdraw");
+						System.out.println("3. Generate Pin");
+						System.out.println("4. Exit");
+						
+						System.out.print("Enter choice: ");
+						int choice1 = sc.nextInt();
 
-                    } else {
-                        System.out.println("Invalid Credentials!");
-                    }
-                    break;
+						switch (choice1) {
 
-                case 3:
-                    System.exit(0);
-            }
-        }
-    }
+						case 1:
+							System.out.print("Account No: ");
+							int acc1 = sc.nextInt();
+
+							System.out.print("Amount: ");
+							double amt1 = sc.nextDouble();
+
+							BankService bn1 = new BankService();
+							bn1.deposit(acc1, amt1);
+							break;
+
+						case 2:
+							System.out.print("Account No: ");
+							int acc2 = sc.nextInt();
+
+							System.out.print("PIN: ");
+							int pin = sc.nextInt();
+
+							System.out.print("Amount: ");
+							double amt2 = sc.nextDouble();
+
+							BankService bn2 = new BankService();
+							bn2.withdraw(acc2, pin, amt2);
+							break;
+
+						case 3:
+							System.out.print("Set PIN: ");
+							int pin2 = sc.nextInt();
+
+							dao.createAccount(userId, pin2);
+							System.out.println("Account Created!");
+
+						case 4:
+							System.out.println("Logging out...");
+							System.exit(0);
+						}
+					}
+
+				} else {
+					System.out.println("Invalid Credentials!");
+				}
+				break;
+
+			case 3:
+				System.exit(0);
+			}
+		}
+	}
 }
+
+
+
